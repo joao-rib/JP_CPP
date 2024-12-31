43ea9cbdbfe1d8be1515feffcf90a1b276f76361
Joao Tiago Ribeiro Pereira <joao-rib@c2r6s9.42porto.com>
2024-12-31 17:38:23 +0000

    2024.12.31 - ex00 done

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <locale>
+
+std::string capitalize(std::string str)
+{
+	for (unsigned int i = 0; i < str.length(); i++)
+	{
+		if (std::isalpha(str[i]))
+			str[i] = std::toupper(str[i]);
+	}
+	return (str);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		for (int i = 1; argv[i]; i++)
+			std::cout << capitalize(argv[i]);
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
+}
