4ea6d3baea549b4099042e6e51704546dfbeb53d
joao-rib <jonnasn@gmail.com>
2025-01-09 01:21:06 +0000

    2025.01.08 - phonebook menu

--- CPP_00/ex01/main.cpp
+++ CPP_00/ex01/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <locale>
+#include "Phonebook.hpp" //WIP create PhoneBook header
+#include "Contacts.hpp" //WIP create Contact header
 
 std::string capitalize(std::string str)
 {
@@ -11,15 +13,42 @@ std::string capitalize(std::string str)
 	return (str);
 }
 
+void print_prompt(void)
+{
+	std::cout << "1 - Enter \"ADD\" in order to add a new contact;\n";
+	std::cout << "2 - Enter \"SEARCH\" in order to display one of your contacts;\n";
+	std::cout << "3 - Enter \"EXIT\" in order to exit this phonebook. Forever." << std::endl;
+}
+
 int	main(void)
 {
-	if (argc > 1)
+	system("clear");
+	std::cout << "Thank you for opening a new phonebook! :D They're rare these days\n\n";
+	print_prompt();
+	std::string input;
+	Contact contact; //WIP create Contact class
+	PhoneBook phonebook; //WIP create PhoneBook class
+	while (std::getline(std::cin, input))
 	{
-		for (int i = 1; argv[i]; i++)
-			std::cout << capitalize(argv[i]);
-		std::cout << std::endl;
+		capitalize(input);
+		if (input == "EXIT")
+		{
+			std::cout << "Thank you for using our services :) Bye!" << std::endl;
+			return (0);
+		}
+		else if (input == "ADD")
+		{
+			system("clear");
+			; // WIP Let's add
+		}
+		else if (input == "SEARCH")
+		{
+			system("clear");
+			; // WIP Let's search
+		}
+		else
+			std::cout << "Command not found ¯\\_(ツ)_/¯ Try something else\n" << std::endl;
+		print_prompt();
 	}
-	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	return (0);
 }
