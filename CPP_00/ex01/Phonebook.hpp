757a1422abb52aef1d2d5ba803213a29d2fee28a
joao-rib <jonnasn@gmail.com>
2025-01-10 02:06:27 +0000

    2025.01.09 - ex01 only needs Search

--- CPP_00/ex01/Phonebook.hpp
+++ CPP_00/ex01/Phonebook.hpp
@@ -21,22 +21,4 @@ public:
 	PhoneBook() : num_contacts(0) {};
 	void add_contact(Contact new_contact);
     std::string itoa(int num);
-}
-/*{
-private:
-	Contact contacts[8];
-	int n_contacts;
-public:
-	PhoneBook() : n_contacts(0) {};
-    int get_ncontacts() const;
-    void add_contact(Contact new_contact);
-    Contact& get_contact(int index);
-    void show_phonebook(void);
-    void phonebook_header(void);
-    int display_contacts(int index);
-    int is_digit(int c);
-    int	stoi(std::string input);
-    int	not_a_digit(std::string input);
-    void utils(void);
-    int newheader(void);
-}*/;
+};
