9cfd622aeee996b4f2fba43aef29b8fd7e4f5b50
joao-rib <jonnasn@gmail.com>
2025-02-07 13:52:08 +0000

    2025.02.07 - microshell practice

---
+++ microshell/main.c
@@ -0,0 +1,19 @@
+//Allowed functions:
+//    malloc, free, write, close, fork, waitpid, signal, kill, exit, chdir, execve, dup, dup2, pipe, strcmp, strncmp
+
+int	main(int argc, char **argv)
+{
+	//Parse - tokens (argv)
+	//Parse - Build tree
+	//Execute commands
+		//Write cd
+		//execve()
+
+    //Assumir caso simples
+    	//execve()
+    //Else if ";"
+    	//??? (estudar o que faz no Bash)
+    //Else if "|"
+    	//fork()
+	return (0);
+}
