6971f5d9019c57f2fd7262688465c50a3d92a270
joao-rib <jonnasn@gmail.com>
2025-02-08 21:00:43 +0000

    2025.02.08 - microshell suggestions

--- microshell/main.c
+++ microshell/main.c
@@ -72,3 +72,57 @@ int	main(int argc, char **argv, char **envp)
     	//??? (estudar o que faz no Bash)
     //Else if "|"
     	//fork()
+
+
+/*
+#include "../include/minishell.h"
+
+void	change_dir(char *path, t_minish *ms)
+{
+	char	*oldpwd;
+	char	*newpwd;
+
+	if (!path)
+	{
+		error("minishell: cd: HOME not set\n", 1);
+		return ;
+	}
+	oldpwd = ft_strjoin("OLDPWD=", ms->cwd);
+	add_or_update_env(&ms->env_list, oldpwd);
+	add_or_update_env(&ms->env_tmp, oldpwd);
+	free(oldpwd);
+	chdir(path);
+	free(ms->cwd);
+	ms->cwd = getcwd(NULL, 4096);
+	newpwd = ft_strjoin("PWD=", ms->cwd);
+	add_or_update_env(&ms->env_list, newpwd);
+	add_or_update_env(&ms->env_tmp, newpwd);
+	free(newpwd);
+	free(path);
+}*/
+/*
+void	cd(char **tokens, t_minish *ms)
+{
+	struct stat	stats;
+	int			i;
+
+	i = 0;
+	while (tokens[i])
+		i++;
+	if (i > 1)
+	{
+		error("minishell: cd: too many arguments\n", 1);
+		return ;
+	}
+	if (!tokens[0][0] || ft_str_cmp(tokens[0], "~"))
+	{
+		change_dir(get_env("HOME", ms->env_list), ms);
+		return ;
+	}
+	if (stat(tokens[0], &stats) == 0 && S_ISDIR(stats.st_mode))
+		change_dir(ft_strdup(tokens[0]), ms);
+	else
+		error("minishell: cd: not a directory\n", 1);
+	unlink_hd_file(ms);
+}
+*/
