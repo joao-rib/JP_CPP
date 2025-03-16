08099bf09c50007eda5ad4586ea4061a65a79bb3
Joao Tiago Ribeiro Pereira <joao-rib@c2r1s6.42porto.com>
2025-03-16 14:15:14 +0000

    2025.03.16 - CPP03 begins

---
+++ CPP_03/ex00/ClapTrap.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <iostream>
+#include <locale>
+#include <iomanip>
+#include <cstdlib>
+
+class Fixed
+{
+private:
+	int _num;
+	const static int _bits = 8;
+public:
+	Fixed();
+	Fixed(const Fixed &orig);
+	Fixed &operator = (const Fixed &orig);
+	~Fixed();
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+};
+
+#endif
