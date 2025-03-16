b3a74bf6d86b3955ce01f38509dd84f18ea239cc
Joao Tiago Ribeiro Pereira <joao-rib@c2r1s6.42porto.com>
2025-03-16 13:16:55 +0000

    2025.03.15 - BSP is done

--- CPP_02/ex03/bsp.cpp
+++ CPP_02/ex03/bsp.cpp
@@ -18,20 +18,29 @@ Fixed distX(Point const p1, Point const p2)
 
 Fixed triangle_area(Point const a, Point const b, Point const c)
 {
-	Fixed left = Fixed::min(a.getX(), Fixed::min(b.getX(), c.getX()));
-	Fixed right = Fixed::max(a.getX(), Fixed::max(b.getX(), c.getX()));
-	Fixed bottom = Fixed::min(a.getY(), Fixed::min(b.getY(), c.getY()));
-	Fixed top = Fixed::max(a.getY(), Fixed::max(b.getY(), c.getY()));
-
-	return ((top - bottom) * (right - left) / 2);
+//	Fixed left = Fixed::min(a.getX(), Fixed::min(b.getX(), c.getX()));
+//	Fixed right = Fixed::max(a.getX(), Fixed::max(b.getX(), c.getX()));
+//	Fixed bottom = Fixed::min(a.getY(), Fixed::min(b.getY(), c.getY()));
+//	Fixed top = Fixed::max(a.getY(), Fixed::max(b.getY(), c.getY()));
+	Fixed tri_area = (a.getX() * (b.getY() - c.getY())) + (b.getX() * (c.getY() - a.getY()))
+					+ (c.getX() * (a.getY() - b.getY()));
+	if (tri_area < 0)
+		tri_area = tri_area * (-1);
+	tri_area = tri_area / 2;
+	return (tri_area);
 }
 
 bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
 	Fixed bsp_tri_area = triangle_area(a, b, c);
-	Fixed point_area;
+	Fixed point_area1 = triangle_area(a, b, point);
+	Fixed point_area2 = triangle_area(a, point, c);
+	Fixed point_area3 = triangle_area(point, b, c);
 
-	point_area = triangle_area(a, b, point) + triangle_area(a, point, c) + triangle_area(point, b, c);
+	if (point_area1 == 0 || point_area2 == 0 || point_area3 == 0)
+		return (false);
+
+	Fixed point_area = point_area1 + point_area2 + point_area3;
 	std::cout << bsp_tri_area << std::endl;
 	std::cout << point_area << std::endl;
 	if (point_area == bsp_tri_area)
--- CPP_02/ex03/main.cpp
+++ CPP_02/ex03/main.cpp
@@ -2,45 +2,13 @@
 
 int main (void)
 {
-	Point a(0, 0);
-	Point b(5, 0);
-	Point c(2, 4);
-	Point d(2, 1);
+	Point a(5, 2);
+	Point b(5, 8);
+	Point c(11, 2);
+	Point d(8, 2.01);
 	if (bsp(a, b, c, d))
 		std::cout << "Tá dentro do triângulo. Muito bem!" << std::endl;
 	else
 		std::cout << "Errou feio. Errou rude. Tá mal. Tá errado." << std::endl;
 	return (0);
 }
-
-/*int main(void)
-{
-	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	Fixed temp1(5.05f);
-	Fixed temp2(2);
-	Fixed const b = temp1 * temp2;
-	Fixed const c = temp1 + temp2;
-	Fixed const d = temp1 - temp2;
-	Fixed const e = temp1 / temp2;
-
-	std::cout << temp1 << std::endl;
-	std::cout << temp2 << std::endl;
-	std::cout << c << std::endl;
-	std::cout << d << std::endl;
-	std::cout << e << std::endl;
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max(a, b) << std::endl;
-	if (Fixed::max(a, b) == a)
-		std::cout << "a is bigger" << std::endl;
-	else if (b > a)
-		std::cout << "b is bigger, it figures" << std::endl;
-	else
-		std::cout << "b is bigger" << std::endl;
-	return 0;
-}*/
