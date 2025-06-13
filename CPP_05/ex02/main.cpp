c2158f9a3e46c443cfc726ca2b2048a0ad685d7c
joao-rib <jonnasn@gmail.com>
2025-06-13 01:46:54 +0100

    2025.06.12 - Building the main

--- CPP_05/ex02/main.cpp
+++ CPP_05/ex02/main.cpp
@@ -1,5 +1,7 @@
 #include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int	main()
 {
@@ -7,19 +9,31 @@ try
 {
 	Bureaucrat	Matrix("Agent Smith", 1);
 	Bureaucrat	Hitman("Agent 47", 47);
-	Bureaucrat	MIB("J", 150);
+	Bureaucrat	MIB("Agent J", 142);
 	Bureaucrat	goon;
-	ShrubberyCreationForm		scf("Home");
+	ShrubberyCreationForm		scf("Av_Aliados");
+	RobotomyRequestForm			rrf("Coporob");
+	PresidentialPardonForm		ppf("Nelson Mandela");
 
 	std::cout << Matrix << " is on the scene." << std::endl;
 	std::cout << Hitman << " is on the scene." << std::endl;
 	std::cout << MIB << " is on the scene." << std::endl;
 	std::cout << goon << " is on the scene." << std::endl;
-	std::cout << scf << " needs to be filled." << std::endl;
 
+	std::cout << "/n" << scf << " needs to be signed and executed." << std::endl;
 	MIB.signForm(scf);
 	Hitman.signForm(scf);
 	goon.signForm(scf);
+
+	std::cout << "/n" << rrf << " needs to be signed and executed." << std::endl;
+	MIB.signForm(rrf);
+	Hitman.signForm(rrf);
+	goon.signForm(rrf);
+
+	std::cout << "/n" << ppf << " needs to be signed and executed." << std::endl;
+	MIB.signForm(ppf);
+	Hitman.signForm(ppf);
+	goon.signForm(ppf);
 }
 catch (std::exception &e)
 {
