fb7675bcfff1685ace8c4b7d6bcbd2cfd9b4037c
Joao Tiago Ribeiro Pereira <joao-rib@c2r1s6.42porto.com>
2025-07-03 12:07:46 +0100

    2025.07.03 - Stupid test should be commented

--- CPP_07/ex00/main.cpp
+++ CPP_07/ex00/main.cpp
@@ -9,9 +9,10 @@ int	main(void)
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
 	const std::string c = "chaine1";
 	const std::string d = "chaine2";
-	::swap(c, d);
+	//::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
