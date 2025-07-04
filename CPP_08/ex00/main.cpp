12bb32eb97908ca6b82743b3371f045ae986e78f
Joao Tiago Ribeiro Pereira <joao-rib@c2r6s7.42porto.com>
2025-07-04 11:34:54 +0100

    2025.07.04 - Better main

--- CPP_08/ex00/main.cpp
+++ CPP_08/ex00/main.cpp
@@ -6,6 +6,7 @@ int	main(void)
 	//VECTORS
 	std::vector<int>			vec_values;
 	//std::vector<std::string>	vec_names;
+	std::vector<float>			vec_float;
 
 	try {
 		::easyfind(vec_values, 42);
@@ -21,15 +22,19 @@ int	main(void)
 	/*vec_names.push_back("Day");
 	vec_names.push_back("Month");
 	vec_names.push_back("Year");*/
+	vec_float.push_back(21.10);
+	vec_float.push_back(10.00);
+	vec_float.push_back(1989.85);
 
 	try {
-		//::easyfind(vec_names, 10);
 		::easyfind(vec_values, 10);
-		std::cout << "10 was found in vec_values, not in vec_names" << std::endl;
+		//::easyfind(vec_names, 10);
+		::easyfind(vec_float, 10);
 	}
 	catch(const std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
+	std::cout << "10 was found in vec_values, not in vec_names or vec_float." << std::endl;
 
 	//LISTS
 	std::list<int>				list_values;
@@ -45,11 +50,11 @@ int	main(void)
 	try {
 		//::easyfind(list_names, 10);
 		::easyfind(list_values, 10);
-		std::cout << "10 was found in list_values, not in list_names" << std::endl;
 	}
 	catch(const std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
+	std::cout << "10 was found in list_values, not in list_names." << std::endl;
 
 	return 0;
 }
