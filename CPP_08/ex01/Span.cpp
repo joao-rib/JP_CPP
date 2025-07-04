1fb665fd0bcf98be2cf9a322be7e49f19fdb2cbd
Joao Tiago Ribeiro Pereira <joao-rib@c2r6s7.42porto.com>
2025-07-04 15:37:31 +0100

    2025.07.04 - ex01 ready for testing

--- CPP_08/ex01/Span.cpp
+++ CPP_08/ex01/Span.cpp
@@ -4,23 +4,56 @@
 //HELPER FUNCTIONS
 //
 
+unsigned int	diff(int a, int b)
+{
+	int r = a - b;
+	if (r < 0)
+		r *= -1;
+	return (static_cast<unsigned int>(r));
+}
+
 //
 //MEMBER FUNCTIONS
 //
 
 void	Span::addNumber(int num)
 {
-	try
+	if (this->_int_vec.size() == this->_size)
+		throw FullSpanException(this->_size);
+
+	this->_int_vec.push_back(num);
+	std::cout << num << " has been added to the integer array." << std::endl;
+}
+
+unsigned int	Span::shortestSpan(void) const
+{
+	unsigned int s = this->_int_vec.size();
+
+	if (s <= 1)
+		throw NoSpanException();
+
+	unsigned int r = diff(this->_int_vec[0], this->_int_vec[1]);
+
+	for (unsigned int i = 0; i < s; i++)
 	{
-		if (this->_int_vec.size() == this->N)
-			throw FullSpanException(this->_size);
-		this->_int_vec.push_back(num);
-		std::cout << num << " has been added to the integer array." << std::endl;
+		for (unsigned int j = 0; j < s; j++)
+		{
+			if (diff(_int_vec[i], _int_vec[j]) < r)
+				r = diff(_int_vec[i], _int_vec[j]);
+		}
 	}
-	catch (std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+	return (r);
+}
+
+unsigned int	Span::longestSpan(void) const
+{
+	if (this->_int_vec.size() <= 1)
+		throw NoSpanException();
+
+	std::vector<int>::const_iterator min = std::min_element(_int_vec.begin(), _int_vec.end());
+	std::vector<int>::const_iterator max = std::max_element(_int_vec.begin(), _int_vec.end());
+
+	return (diff(*min, *max));
 }
 
 //
@@ -36,13 +69,13 @@ Span &Span::operator = (const Span &orig)
 	if (this != &orig)
 	{
 		_size = orig._size;
-		_int_vec = orig._int_vect;
+		_int_vec = orig._int_vec;
 	}
 	//std::cout << "Span assignment copy-constructed." << std::endl;
 	return (*this);
 }
 
-Span::Span(const Span &orig): _size(orig._size), _int_vec(orig._int_vec)
+Span::Span(const Span &orig): _int_vec(orig._int_vec), _size(orig._size)
 {
 	//std::cout << "Span copy-constructed." << std::endl;
 }
@@ -70,36 +103,36 @@ Span::~Span(void)
 // EXCEPTIONS
 //
 
-Bureaucrat::FullSpanException::FullSpanException(unsigned int N)
+Span::FullSpanException::FullSpanException(unsigned int N)
 {
 	std::ostringstream out;
 	out << "Cannot add more than " << N << " integers.";
 	_msg = out.str();
 }
 
-Bureaucrat::FullSpanException::~FullSpanException() throw()
+Span::FullSpanException::~FullSpanException() throw()
 {
 	//std::cout << "Error message destroyed" << std::endl;
 }
 
-const char *Bureaucrat::FullSpanException::what() const throw()
+const char *Span::FullSpanException::what() const throw()
 {
 	return (this->_msg.c_str());
 }
 
-Bureaucrat::NoSpanException::NoSpanException()
+Span::NoSpanException::NoSpanException()
 {
 	std::ostringstream out;
 	out << "Two integers are needed to find Span.";
 	_msg = out.str();
 }
 
-Bureaucrat::NoSpanException::~NoSpanException() throw()
+Span::NoSpanException::~NoSpanException() throw()
 {
 	//std::cout << "Error message destroyed" << std::endl;
 }
 
-const char *Bureaucrat::NoSpanException::what() const throw()
+const char *Span::NoSpanException::what() const throw()
 {
 	return (this->_msg.c_str());
 }
