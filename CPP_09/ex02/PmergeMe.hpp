ccaf20c9abbdf0b2d6364d149e19dcfea0ec7030
Joao Tiago Ribeiro Pereira <joao-rib@c2r1s6.42porto.com>
2025-07-28 15:41:21 +0100

    2025.07.28 - Template gameplan is on

--- CPP_09/ex02/PmergeMe.hpp
+++ CPP_09/ex02/PmergeMe.hpp
@@ -45,7 +45,7 @@ private:
 	std::deque<int>		order_deque(std::deque<int> above);
 
 	template <typename T>
-	void order_container(T& container, unsigned int layer, unsigned int pair_total); // WIP
+	void order_container(T& container, unsigned int layer); // WIP
 	unsigned int	_last_layer;
 public:
 	PmergeMe();
@@ -72,6 +72,14 @@ public:
 };
 
 template<typename T>
+void	swap(T& a, T& b)
+{
+	T buff = a;
+	a = b;
+	b = buff;
+}
+
+template<typename T>
 T&	min(T& a, T& b)
 {
 	if (a < b)
@@ -88,10 +96,12 @@ T&	max(T& a, T& b)
 }
 
 template<typename T>
-void PmergeMe::order_container(T& container, unsigned int layer, unsigned int pair_total) // WIP
+void PmergeMe::order_container(T& container, unsigned int layer) // WIP
 {
-	size_t s = container.size();
-	// int pair_total = s / (2 * layer); // Odd numbers unaccounted for
+	size_t pair_size = layer;
+	for (unsigned int i = 0; i < layer; i++)
+		pair_size *= 2;
+	int pair_total = container.size() / pair_size; // Odd numbers unaccounted for?
 
 	// Last layer
 	if (pair_total < 2)
@@ -101,23 +111,60 @@ void PmergeMe::order_container(T& container, unsigned int layer, unsigned int pa
 	}
 
 	// If Container's size is odd
-	/*int Odd_Element = -1;
+	int Odd_Element = -1;
 	if (s != pair_total * 2 * layer)
-		Odd_Element = container.back(); // WIP must be the entire pair*/
+		Odd_Element = container.back(); // WIP must be the entire pair
 
-	// Create main and pend
+	// Swap numbers/pairs where needed
+	for (unsigned int i = 0; i < (pair_total * pair_size); i++)
+	{
+		if (container[i] > container[i + 1])
+			::swap(container[i], container[i + 1]); // WIP must be the entire pair
+	}
 
 	// Recurse
+	this->order_container(container, layer + 1);
 
-	//if (Odd_Element > -1)
-	//	pend_cnt.push_back(Odd_Element);
+	// Create main and pend
+	std::T::T pend_cnt;
+	std::T::T main_cnt;
+	T::iterator it = container.begin();
+	main_cnt = push_back(*it); // b1 // WIP must be the entire pair
+	main_cnt = push_back(*(++it)); // a1 // WIP must be the entire pair
+	it++;
+	for (unsigned int i = 2; i < (pair_total * pair_size); i += 2, it++) // WIP must be the entire pair
+	{
+		pend_cnt.push_back(push_back(*it));
+		main_cnt.push_back(push_back(*(++it)));
+	}
 
-	// STEP 3: Jacobsthal WIP
-	// 3.1: Jacobsthal numbers
-	// 3.2: Insert b1, a1, a2, etc. to main
-	// 3.3: Insert pairs from pend into main (Jacobstahl order, in reverse)
-	//  main_vec.insert(std::lower_bound(main_vec.begin(), main_vec.end(), value), value);
-	// 3.4: Insert remaining pends, if any
+	// Add the odd element into the pend
+	if (Odd_Element > -1)
+		pend_cnt.push_back(Odd_Element); // WIP must be the entire pair
+
+	// Insert pend elements into the main
+	int j = 2;
+	size_t c = 0;
+	size_t p = pend_cnt.size();
+	while (c < p)
+	{
+		for (unsigned int i = diffJacobsthal(j); i >= 0; i--)
+		{
+			if (i > p)
+				break ;
+			main_cnt.insert(std::lower_bound(main_cnt.begin(), main_cnt.end(), pend_cnt[i]), pend_cnt[i]); // WIP must be the entire pair
+		}
+		c += diffJacobsthal(j);
+		j++;
+	}
+
+	// Insert remaining pends, if any
+	while (--p >= c)
+		main_cnt.insert(std::lower_bound(main_cnt.begin(), main_cnt.end(), pend_cnt[p]), pend_cnt[p]); // WIP must be the entire pair
+
+	// STEP 4: Replace values in container, from main
+	for (unsigned int i = 0; i < main_cnt.size(); i++)
+		container[i] = main_cnt[i]; // WIP Does it work between pairs and values?
 }
 
 #endif
