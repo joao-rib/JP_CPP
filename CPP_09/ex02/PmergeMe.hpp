5a49255e0c212fa9006a002c51f43a167c2cecb1
Joao Tiago Ribeiro Pereira <joao-rib@c2r4s2.42porto.com>
2025-07-30 16:53:18 +0100

    2025.07.30 - All that remains is reassure the insert_element function

--- CPP_09/ex02/PmergeMe.hpp
+++ CPP_09/ex02/PmergeMe.hpp
@@ -73,32 +73,33 @@ public:
 };
 
 template<typename T>
-void	swap_pairs(T& container, unsigned int start, size_t pair_half_size)
+void	swap_pairs(T& container, unsigned int start, size_t element_size)
 {
-	size_t end = start + pair_half_size;
+	size_t end = start + element_size;
 	while (start < end)
 	{
 		T buff = container[start];
-		container[start] = container[start + pair_half_size];
-		container[start + pair_half_size] = buff;
+		container[start] = container[start + element_size];
+		container[start + element_size] = buff;
 		start++;
 	}
 }
 
 template<typename T>
-void	insert_pairs(T& main_cnt, T& pend_cnt, size_t pos, size_t pair_half_size)
+void	insert_pairs(T& main_cnt, T& pend_cnt, size_t pos, size_t element_size) // WIP
 {
-	typename T::iterator it = std::lower_bound(main_cnt.begin(), main_cnt.end(), pend_cnt[pos * pair_half_size]); // WIP end()?
-	main_cnt.insert(it, pend_cnt[pos * pair_half_size]); // WIP lower_bound is only relevant for highest number
-	for (unsigned int i = 1; i <= pair_half_size; i--)
-		; // WIP lower_bound is only relevant for highest number
+	size_t true_pos = (pos * element_size) + (element_size - 1);
+	typename T::iterator it = std::lower_bound(main_cnt.begin(), main_cnt.end(), pend_cnt[true_pos]) - element_size + 1; // WIP end()?
+
+	for (unsigned int i = 0; i < element_size; i++)
+		main_cnt.insert(it - i, pend_cnt[true_pos - i]);
 }
 
 template<typename T>
-void	push_back_pairs(T& container, T& buff_cnt, size_t pair_num, size_t pair_size)
+void	push_back_pairs(T& container, T& buff_cnt, size_t pair_num, size_t element_size)
 {
-	for (unsigned int i = 0; i < pair_size; i++)
-		buff_cnt.push_back(container[(pair_num * pair_size) + i]);
+	for (unsigned int i = 0; i < element_size; i++)
+		buff_cnt.push_back(container[(pair_num * element_size) + i]);
 }
 
 template<typename T>
@@ -107,22 +108,6 @@ void	push_back_rest(T& container, T& buff_cnt, size_t start_pos)
 	for (unsigned int i = start_pos; i < buff_cnt.size(); i++)
 		buff_cnt.push_back(container[i]);
 }
-
-/*template<typename T>
-T&	min(T& a, T& b)
-{
-	if (a < b)
-		return a;
-	return b;
-}
-
-template<typename T>
-T&	max(T& a, T& b)
-{
-	if (a > b)
-		return a;
-	return b;
-}*/
 
 template<typename T>
 void PmergeMe::order_container(T& container, unsigned int layer) // WIP
@@ -139,8 +124,8 @@ void PmergeMe::order_container(T& container, unsigned int layer) // WIP
 		return ;
 	}
 
-	// If Container's size is odd, then this variable is different from 0
-	unsigned int Odd_Element_pos = this->getSize() - (pair_total * pair_size);
+	// If Container's size is odd, then this variable is smaller than _size
+	unsigned int Odd_Element_pos = pair_total * pair_size;
 
 	// Swap numbers/pairs where needed
 	for (unsigned int i = 0; i < pair_total; i += pair_size)
@@ -155,17 +140,17 @@ void PmergeMe::order_container(T& container, unsigned int layer) // WIP
 	// Create main and pend
 	T pend_cnt;
 	T main_cnt;
-	push_back_pairs(container, main_cnt, 0, pair_size / 2); // b1
-	push_back_pairs(container, main_cnt, 1, pair_size / 2); // a1
+	::push_back_pairs(container, main_cnt, 0, pair_size / 2); // b1
+	::push_back_pairs(container, main_cnt, 1, pair_size / 2); // a1
 	for (unsigned int i = 2; i < (pair_total * pair_size); i++) 
 	{
-		push_back_pairs(container, pend_cnt, i, pair_size / 2); // Remaining b
-		push_back_pairs(container, main_cnt, ++i, pair_size / 2); // Remaining a
+		::push_back_pairs(container, pend_cnt, i, pair_size / 2); // Remaining b
+		::push_back_pairs(container, main_cnt, ++i, pair_size / 2); // Remaining a
 	}
 
 	// Add the odd elements into the pend
-	if (Odd_Element_pos > 0)
-		push_back_rest(container, pend_cnt, Odd_Element_pos);
+	if (Odd_Element_pos < this->getSize())
+		::push_back_rest(container, pend_cnt, Odd_Element_pos);
 
 	// Insert pend elements into the main
 	int j = 2;
@@ -173,23 +158,27 @@ void PmergeMe::order_container(T& container, unsigned int layer) // WIP
 	size_t p = pend_cnt.size() / (pair_size / 2);
 	while (c < p)
 	{
-		for (unsigned int i = diffJacobsthal(j); i >= 0; i--)
+		for (unsigned int i = diffJacobsthal(j); i > 0; i--)
 		{
-			if (i > p)
+			if ((c + i) > p)
 				break ;
-			insert_pairs(main_cnt, pend_cnt, c + i, pair_size); // WIP must be the entire pair
+			::insert_pairs(main_cnt, pend_cnt, c + i - 1, pair_size / 2);
 		}
 		c += diffJacobsthal(j);
 		j++;
 	}
 
 	// Insert remaining pends, if any
-	/*while (--p >= c)
-		insert_pairs(main_cnt, pend_cnt[p], pair_size); // WIP must be the entire pair
+	for (unsigned int i = p - c; i > 0; i--)
+		::insert_pairs(main_cnt, pend_cnt, c + i - 1, pair_size / 2);
+
+	// Add remaining elements into the pend
+	if (p * (pair_size / 2) < pend_cnt.size())
+		::push_back_rest(main_cnt, pend_cnt, p * (pair_size / 2));
 
 	// Replace values in container, from main
 	for (unsigned int i = 0; i < main_cnt.size(); i++)
-		container[i] = main_cnt[i]; // Should work...*/
+		container[i] = main_cnt[i];
 }
 
 #endif
